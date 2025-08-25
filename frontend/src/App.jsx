0fb6427241741fe506ed619c88fefdc1b7a154ca
SK sanoyar ali <asksanoyar33@gmail.com>
2025-08-25 23:21:09 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center text-green-500">
+        Lets build netflix app
+      </h1>
+    </div>
+  )
+}
+export default App
